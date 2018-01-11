c108d9526919b659493e9334a732819ddbad8b39
Dhiman <dhimam@amazon.com>
2018-01-11 23:43:28 +0530

    added

---
+++ MISC/SuperMethod.py
@@ -0,0 +1,13 @@
+class Mammal(object):
+    def __init__(self, mammalName):
+        print(mammalName, 'is a warm-blooded animal.')
+
+
+class Dog(Mammal):
+    def __init__(self):
+        super(Dog, self).__init__('Dog')
+        print('Dog has four legs.')
+
+
+
+d1 = Dog()
